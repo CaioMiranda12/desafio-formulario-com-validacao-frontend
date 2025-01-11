cb9635a29ea73dcaa58d4e378a63b9df8fcc5a8d
CaioMiranda12 <caioclm04@gmail.com>
2025-01-11 11:38:28 -0300

    add react hook form and validation with yup

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,34 @@
 import { useState } from "react";
+import { useForm } from "react-hook-form"
+import { yupResolver } from "@hookform/resolvers/yup"
+import * as yup from "yup"
 
 function App() {
   const [openLinkedin, setOpenLinkedin] = useState(false)
   const [openGithub, setOpenGithub] = useState(false)
+
+  const schema = yup.object({
+    name: yup.string().required('O seu nome é obrigatório'),
+    email: yup
+      .string()
+      .email('Digite um e-mail válido')
+      .required('O e-mail é obrigatório'),
+    fone: yup.string().required('O seu telefone é obrigatório'),
+    linkedin: yup.string(),
+    github: yup.string()
+  }).required()
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  })
+  const onSubmit = (data) => {
+    console.log(data)
+  }
+
 
   return (
     <>
@@ -10,14 +36,16 @@ function App() {
         <div className='flex flex-col-reverse sm:grid sm:grid-cols-2 sm:gap-12 sm:w-11/12 lg:w-10/12 p-4 sm:p-8 border border-gray-300'>
           <div>
 
-            <div className='mt-5 flex flex-col gap-4'>
+            <form className='mt-5 flex flex-col gap-4' onSubmit={handleSubmit(onSubmit)}>
               <div className='flex flex-col gap-2'>
                 <label className='text-white'>Nome completo</label>
                 <input
                   type="text"
                   placeholder="Digite seu nome..."
                   className='bg-transparent placeholder:text-gray-300 text-white border py-1 px-2 border-gray-200'
+                  {...register("name")}
                 />
+                <p className="text-red-500 font-bold">{errors.name?.message}</p>
               </div>
 
               <div className='flex flex-col gap-2'>
@@ -25,8 +53,11 @@ function App() {
                 <input
                   type="email"
                   placeholder="Digite seu e-mail..."
+
                   className='bg-transparent placeholder:text-gray-300 text-white border py-1 px-2 border-gray-200'
+                  {...register("email")}
                 />
+                <p className="text-red-500 font-bold">{errors.email?.message}</p>
               </div>
 
               <div className='flex flex-col gap-2'>
@@ -35,70 +66,72 @@ function App() {
                   type="text"
                   placeholder="Digite seu telefone..."
                   className='bg-transparent placeholder:text-gray-300 text-white border py-1 px-2 border-gray-200'
+                  {...register("fone")}
                 />
+                <p className="text-red-500 font-bold">{errors.fone?.message}</p>
               </div>
 
               <div className='flex flex-col gap-2'>
                 <label className='text-white'>Cargo</label>
 
                 <select className='h-8'>
-                  <option value="#">
+                  <option value="Frontend">
                     Frontend
                   </option>
 
-                  <option value="#">
+                  <option value="Backend">
                     Backend
                   </option>
 
-                  <option value="#">
+                  <option value="Full Stack">
                     Full Stack
                   </option>
 
-                  <option value="#">
+                  <option value="Desenvolvedor Mobile">
                     Desenvolvedor Mobile
                   </option>
 
-                  <option value="#">
+                  <option value="Desenvolvedor de Software">
                     Desenvolvedor de Software
                   </option>
 
-                  <option value="#">
+                  <option value="Engenheiro de Software">
                     Engenheiro de Software
                   </option>
 
-                  <option value="#">
+                  <option value="Arquiteto de Software">
                     Arquiteto de Software
                   </option>
 
-                  <option value="#">
+                  <option value="UI/UX Designer">
                     UI/UX Designer
                   </option>
 
-                  <option value="#">
+                  <option value="Analista de Sistemas">
                     Analista de Sistemas
                   </option>
 
-                  <option value="#">
+                  <option value="Analista Programador">
                     Analista Programador
                   </option>
 
-                  <option value="#">
+                  <option value="DevOps Engineer">
                     DevOps Engineer
                   </option>
 
-                  <option value="#">
+                  <option value="Engenheiro de Dados">
                     Engenheiro de Dados
                   </option>
 
-                  <option value="#">
+                  <option value="QA Engineer">
                     QA Engineer
                   </option>
 
-                  <option value="#">
+                  <option value="Scrum Master">
                     Scrum Master
                   </option>
 
-                  <option value="#">
+                  <option value="Product Owner">
                     Product Owner
                   </option>
                 </select>
@@ -116,11 +149,15 @@ function App() {
                 </div>
 
                 {openLinkedin && (
-                  <input
-                    type="text"
-                    placeholder="Digite a url do linkedin..."
-                    className='bg-transparent placeholder:text-gray-300 text-white border py-1 px-2 border-gray-200'
-                  />
+                  <div className="flex flex-col">
+                    <input
+                      type="text"
+                      placeholder="Digite a url do linkedin..."
+                      className='bg-transparent placeholder:text-gray-300 text-white border py-1 px-2 border-gray-200'
+                      {...register("linkedin")}
+                    />
+                    <p className="text-red-500 font-bold">{errors.linkedin?.message}</p>
+                  </div>
                 )}
               </div>
 
@@ -132,21 +169,31 @@ function App() {
                     checked={openGithub}
                     onChange={(e) => setOpenGithub(e.target.checked)}
                     className="w-4 h-4"
+
                   />
                 </div>
 
                 {openGithub && (
-                  <input
-                    type="text"
-                    placeholder="Digite a url do github..."
-                    className='bg-transparent placeholder:text-gray-300 text-white border py-1 px-2 border-gray-200'
-                  />
+                  <div className="flex flex-col">
+                    <input
+                      type="text"
+                      placeholder="Digite a url do github..."
+                      className='bg-transparent placeholder:text-gray-300 text-white border py-1 px-2 border-gray-200'
+                      {...register("github")}
+                    />
+                    <p className="text-red-500 font-bold">{errors.github?.message}</p>
+                  </div>
                 )}
               </div>
 
-              <button className="bg-black text-white p-3 font-bold hover:text-black hover:bg-white duration-500 transition-all">Enviar</button>
+              <button
+                className="bg-black text-white p-3 font-bold hover:text-black hover:bg-white duration-500 transition-all"
+                type="submit"
+              >
+                Enviar
+              </button>
 
-            </div>
+            </form>
 
           </div>
 
